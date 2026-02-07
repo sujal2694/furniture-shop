122e32d35e83024c9ace62d8956e0d97bd382af0
Sujal Valsur <sujalvalsur11@gmail.com>
2026-02-07 21:37:11 +0530

    update

--- client/src/assets/assets.js
+++ client/src/assets/assets.js
@@ -2,6 +2,18 @@ import hero_img from './hero-img.jpeg'
 import cross_icon from './cross_icon.png'
 import profile_icon from './profile_icon.png'
 import search_icon from './search_icon.png'
+import sofa_img_1 from './sofa-1.jpg'
+import sofa_img_2 from './sofa-2.jpg'
+import sofa_img_3 from './sofa-3.jpg'
+import sofa_img_4 from './sofa-4.jpg'
+import bed_img_1 from './bed-1.jpg'
+import bed_img_2 from './bed-2.jpg'
+import bed_img_3 from './bed-3.jpg'
+import chair_img_1 from './chair-1.webp'
+import chair_img_2 from './chair-2.webp'
+import table_lamp_img_1 from './table-lamp-1.webp'
+import table_lamp_img_2 from './table-lamp-2.webp'
+import table_lamp_img_3 from './table-lamp-3.webp'
 
 
 export const assets = {
