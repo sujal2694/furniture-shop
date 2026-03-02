2c0abf95432e8bc0bc38a19a499877f7b8006b12
Sujal Valsur <sujalvalsur11@gmail.com>
2026-03-02 21:48:43 +0530

    update

--- client/src/Pages/Contact/Contact.jsx
+++ client/src/Pages/Contact/Contact.jsx
@@ -1,10 +1,22 @@
 import React from 'react';
+import './Contact.css'
 
 const Contact = () => {
   return (
-    <div style={{padding: '48px', textAlign: 'center'}}>
-      <h1>Contact Us</h1>
-      <p style={{maxWidth:700, margin:'18px auto', color:'#444'}}>Contact page placeholder — replace with your contact form or details.</p>
+    <div className='contact-us'>
+      <h1>Contact</h1>
+      <form className="contact-form">
+        <div className='multi-fields'>
+          <input type="text" placeholder='first name' />
+          <input type="text" placeholder='last name' />
+        </div>
+        <textarea cols={40} rows={5} placeholder='address type here...'></textarea>
+        <div className='multi-fields'>
+          <input type="text" placeholder='mobile no.' />
+          <input type="text" placeholder='city' />
+        </div>
+        <button type='submit' className='contact-submit-btn'>Submit</button>
+      </form>
     </div>
   )
 }
